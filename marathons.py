179096a5f5bcb8a4bc9354fb91fbdb5404fbf112
Ayoub_Sikouky <aysikouky@gmail.com>
2023-06-13 23:56:08 +0100

    Updated "get_max_pages" in HamburgMarathon since marathons after 2017 have their max page as the second to last anchor tag in the page.

--- marathons.py
+++ marathons.py
@@ -333,9 +333,7 @@ class HamburgMarathon(MarathonBase):
         return super().create_soup(webpage_content)
 
     def get_max_pages(
-        self,
-        year: str,
-        num_results: str = "25",
+        self, year: str, num_results: str = "25", after_17: bool = False
     ) -> list[str]:
         """
         ### Method used for getting the max page number for both men and women result pages.
@@ -350,5 +348,9 @@ class HamburgMarathon(MarathonBase):
         men_soup = self.create_soup(webpage_content=web_pages[0].content)
         women_soup = self.create_soup(webpage_content=web_pages[1].content)
 
-        # The fifth Anchor tag starting from last is the total number of pages.
-        return (men_soup.findAll("a")[-5].text, women_soup.findAll("a")[-5].text)
+        if after_17:
+            # Anchor tag before last is the total number of pages.
+            return (men_soup.findAll("a")[-2].text, women_soup.findAll("a")[-2].text)
+        else:
+            # The fifth Anchor tag starting from last is the total number of pages.
+            return (men_soup.findAll("a")[-5].text, women_soup.findAll("a")[-5].text)
