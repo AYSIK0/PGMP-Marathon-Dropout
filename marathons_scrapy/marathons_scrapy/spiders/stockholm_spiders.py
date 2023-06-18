25c5b49036940a4e29ac1523c3695507ba9dcd80
Ayoub_Sikouky <aysikouky@gmail.com>
2023-06-18 02:24:41 +0100

    Added "parse_split" to Stockholm2122 class.

--- marathons_scrapy/marathons_scrapy/spiders/stockholm_spiders.py
+++ marathons_scrapy/marathons_scrapy/spiders/stockholm_spiders.py
@@ -49,3 +49,36 @@ class Stockholm2122(scrapy.Spider):
                 "(?<=idp=).+?(?=&)", runner.xpath(".//h4/a/@href").get()
             )[0]
             yield item
+
+    def parse_split(self, response):
+        """
+        ### Parse the split result pages.
+        """
+        # Scraping Splits.
+        split_item = StockholmSplitItem()
+        split_item["idp"] = re.findall("(?<=idp=).+?(?=&)", response.url)[0]
+
+        splits = response.xpath('//div[@class="detail-box box-splits"]//tr')
+        keys = StockholmSplitItem.get_split_keys()
+        for i, split in enumerate(splits[1:]):  # 10 rows in each splits table.
+            # check if the time is not estimated.
+            if "estimated" not in split.xpath("@class").get():
+                time = split.xpath("td[2]/text()").get()  # time hh:mm:ss
+                pace = split.xpath("td[4]/text()").get()  # min/km
+                speed = split.xpath("td[5]/text()").get()  # km/h
+            else:
+                time = "-"
+                pace = "-"
+                speed = "-"
+            split_item[keys[i]] = [time, pace, speed]
+
+        if split_item["k_finish"][0] and re.match(
+            "(\d{2}:\d{2}:\d{2})", split_item["k_finish"][0]
+        ):
+            split_item["race_state"] = "Finished"
+
+        # Scraping YOB: year of birth.
+        yob_row = response.xpath('//div[@class="detail-box box-general"]//tr')[5]
+        split_item["yob"] = yob_row.xpath("td[1]/text()").get()
+
+        yield split_item
