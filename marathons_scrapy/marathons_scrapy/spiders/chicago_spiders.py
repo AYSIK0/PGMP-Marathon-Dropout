187f9eb267f450b58ea36d99835949129f7bcd79
Ayoub_Sikouky <aysikouky@gmail.com>
2023-06-26 00:32:46 +0100

    Added all necessary functionality form Chicagp1422 class.

--- marathons_scrapy/marathons_scrapy/spiders/chicago_spiders.py
+++ marathons_scrapy/marathons_scrapy/spiders/chicago_spiders.py
@@ -11,13 +11,14 @@ class Chicago1422(scrapy.Spider):
 
     name = "chicago14_23"
 
-    def __init__(
-        self, urls: list[str], splits: bool = False, first_split_idx: int = 1, **kwargs
-    ):
+    def __init__(self, urls: list[str], splits: bool = False, **kwargs):
         self.urls: str = urls
         self.splits: bool = splits
-        self.first_split_idx = first_split_idx
+        self.first_split_idx = (
+            kwargs.get("first_split_idx") if kwargs.get("first_split_idx") else 1
+        )
         self.year = kwargs.get("year")
+        self.finish_div_idx = 1
         super().__init__()
 
     # Logging info
@@ -29,12 +30,14 @@ class Chicago1422(scrapy.Spider):
     def start_requests(self):
         urls = self.urls
         if self.splits:
-            # TODO Test it
-            # callback = self.parse_split_22 if self.year == "2022" else self.parse_split
-            # for url in urls:
-            #     yield scrapy.Request(url=url, callback=callback)
+            # This solution only works for 2022, find better solution for future marathon.
+            callback = self.parse_split_22 if self.year == "2022" else self.parse_split
+            for url in urls:
+                yield scrapy.Request(url=url, callback=callback)
             pass
         else:
+            # This is needed since the finish_time between 2014 - 2021 is //div[1]; for 2022 it is //div[2]
+            self.finish_div_idx = 2 if self.year == "2022" else 1
             for url in urls:
                 yield scrapy.Request(url=url, callback=self.parse)
 
@@ -53,9 +56,75 @@ class Chicago1422(scrapy.Spider):
             ).get()
             item["gender"] = re.findall(".(?=&num)", response.url)[0]
             item["finish"] = runner.xpath(
-                './/div[@class="pull-right"]/div/div[1]/text()'
+                f'.//div[@class="pull-right"]/div/div[{self.finish_div_idx}]/text()'
             ).get()
             item["idp"] = re.findall(
                 "(?<=idp=).+?(?=&)", runner.xpath(".//h4/a/@href").get()
             )[0]
             yield item
+
+    def parse_split(self, response):
+        """
+        ### Parse the split result pages.
+        """
+        split_item = ChicagoSplitItem()
+        split_item["idp"] = re.findall("(?<=idp=).+?(?=&)", response.url)[0]
+        splits = response.xpath('//div[@class="detail-box box-splits"]//tr')
+        keys = ChicagoSplitItem.get_split_keys()
+
+        for i, split in enumerate(splits[self.first_split_idx :]):
+            # check if the time is not estimated.
+            if "estimated" not in split.xpath("@class").get():
+                time = split.xpath('td[@class="time"]/text()').get()  # time hh:mm:ss
+                pace = split.xpath(
+                    'td[contains(@class, "min_km")]/text()'
+                ).get()  # min/km
+                speed = split.xpath('td[contains(@class, "kmh")]/text()').get()  # km/h
+            else:
+                time = "-"
+                pace = "-"
+                speed = "-"
+            split_item[keys[i]] = [time, pace, speed]
+
+        if (
+            # Check if runners have finish split (including empty one).
+            split_item.get("k_finish")
+            # Check if time is available (some runners might have finish speed or pace but no time).
+            and split_item["k_finish"][0]
+            and re.match("(\d{2}:\d{2}:\d{2})", split_item["k_finish"][0])
+        ):
+            split_item["race_state"] = "Finished"
+            split_item["last_split"] = "Finish"
+
+        yield split_item
+
+    def parse_split_22(self, response):
+        """
+        ### Parse the split result pages for 2022.
+        """
+        split_item = ChicagoSplitItem()
+        split_item["idp"] = re.findall("(?<=idp=).+?(?=&)", response.url)[0]
+        split_item["race_state"] = response.xpath(
+            '//div[@class="detail-box box-state"]//tr[1]/td/text()'
+        ).get()
+        split_item["last_split"] = response.xpath(
+            '//div[@class="detail-box box-state"]//tr[2]/td/text()'
+        ).get()
+
+        splits = response.xpath('//div[@class="detail-box box-splits"]//tr')
+        keys = ChicagoSplitItem.get_split_keys()
+
+        for i, split in enumerate(splits[self.first_split_idx :]):
+            if "estimated" not in split.xpath("@class").get():
+                time = split.xpath('td[@class="time"]/text()').get()  # time hh:mm:ss
+                pace = split.xpath(
+                    'td[contains(@class, "min_km")]/text()'
+                ).get()  # min/km
+                speed = split.xpath('td[contains(@class, "kmh")]/text()').get()  # km/h
+            else:
+                time = "-"
+                pace = "-"
+                speed = "-"
+            split_item[keys[i]] = [time, pace, speed]
+
+        yield split_item
