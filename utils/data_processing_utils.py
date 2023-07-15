fbdc5788751d0d8c3e349e630f6a28dcab5f9d6a
Ayoub_Sikouky <aysikouky@gmail.com>
2023-07-15 19:01:34 +0100

    Added:     1 + stockholm_cleaner() function to handle the cleaning of stockholm marathon data.     2 + calc_age() function to calculate the age from year of birth.     3 + get_age_cat() function to get age_cat from age.
    Updated:
        1 + Documentation.

--- utils/data_processing_utils.py
+++ utils/data_processing_utils.py
@@ -38,7 +38,7 @@ def london_cleaner(
         f"Original rows count: {rows_count} || New rows count: {len(df)} || Dropped Rows: {rows_count - len(df)}"
     )
 
-    # 3. Dropping runners that do not have a non-null value in these columns [age_cat, gender, last_split]
+    # 3. Dropping runners that have a null value in these columns [age_cat, gender, last_split]
     df = drop_null_by_col(df, ["age_cat", "gender", "last_split"])
 
     # 4. Replace the characters that match `regex_pattern` by the `replace_value`. N.B Works but Slow.
@@ -82,7 +82,7 @@ def hamburg_cleaner(
     5. The time and pace for each split in `splits_keys` are converted into seconds.
     6. The time, pace, and speed for each split in `splits_keys` dtype are converted to `float32`.
     7. Adding the `last_split` and `race_state` columns.
-    8. Replacing `last_split` and `last_split` values with the standard ones.
+    8. Replacing `age_cat` and `last_split` values with the standard values.
     9. Reordering the DataFrame columns according to cols_order.
     10. Convert columns into best possible dtype using `convert_dtypes()`.
     """
@@ -105,7 +105,7 @@ def hamburg_cleaner(
         f"Original rows count: {rows_count} || New rows count: {len(df)} || Dropped Rows: {rows_count - len(df)}"
     )
 
-    # 4. Dropping runners that do not have a non-null value in these columns [age_cat, gender]
+    # 4. Dropping runners that have a null value in these columns [age_cat, gender]
     df = drop_null_by_col(df, ["age_cat", "gender"])
 
     # 5. Converting time and pace into seconds.
@@ -120,7 +120,7 @@ def hamburg_cleaner(
         df["last_split"] == "k_finish_time", "Finished", "Started"
     )
 
-    # 8. Replacing `last_split` and `last_split` values with the standard ones.
+    # 8. Replacing `age_cat` and `last_split` values with the standard values.
     age_cat_std = get_ham_age_cat_dict(df)
     df["age_cat"] = df["age_cat"].replace(age_cat_std)
     df["last_split"] = df["last_split"].replace(last_split_std)
@@ -129,6 +129,106 @@ def hamburg_cleaner(
     df = df[cols_order]
 
     # 10. Convert columns into best possible dtypes (dtypes are inferred).
+    df = df.convert_dtypes()
+
+    return df
+
+
+def stockholm_cleaner(
+    df: pd.DataFrame,
+    splits_keys: list[str],
+    cols_to_drop: list[str],
+    last_split_std: dict,
+    cols_order: list[str],
+    year: int,
+) -> pd.DataFrame:
+    """
+    ### Function to clean Stockholm marathons' data.
+    #### `N.B` A copy of the original DataFrame is returned after all operations are performed.
+    ----
+    ### Arguments:
+    + df: DataFrame with data to convert.
+    + splits_keys: Name of split columns.
+    + cols_to_drop: Name of columns to remove from the DataFrame.
+    + last_split_std: Dictionary of key value pair of old last_split column names `key` and the new ones `values`.
+    + cols_order: The order of the returned DataFrame columns, it's a list of columns' names arranged in the desired order.
+    + year: The marathon year.
+    ----
+    ### Returns a new DataFrame after applying the operations below.
+    1. Columns in `cols_to_drop` are removed.
+    2. Replacing `['-', ', -, {SPACE}]` with an empty character.
+    3. Remove runners that did not start the marathon `All the splits columns values are None`.
+    4. Dropping runners that do not have a non-null value in these columns `[age_cat, gender, last_split]`.
+    5. The time and pace for each split in `splits_keys` are converted into seconds.
+    6. The time, pace, and speed for each split in `splits_keys` dtype are converted to `float32`.
+    7. Adding the `last_split` column and updating `race_state` column.
+    8. Steps:
+    + 8.1 Calculate the age based on the year of birth.
+    + 8.2 Dropping any non-adult runner age < 18.
+    + 8.3 Get the appropriate age category.
+    + 8.4 the column named `yob` changed to `age_cat`.
+    9. Replacing `last_split` values with the standard values.
+    10. Reordering the DataFrame columns according to cols_order.
+    11. Convert columns into best possible dtype using `convert_dtypes()`.
+    """
+    df = df.copy()
+    # 1. Removing unused columns.
+    if cols_to_drop and len(cols_to_drop) >= 1:
+        df.drop(cols_to_drop, axis=1, inplace=True)
+
+    # 2. Replace the characters match `regex_pattern` by the `replace_value`. N.B Works but Slow.
+    df = replace_value_in_cols(df)
+
+    # 3. Removing runners did not start. (if all splits columns are null then the runner did not start.)
+    print("** Removing Runners That did not start:")
+    rows_count = len(df)
+    not_started_indices = df[
+        df.iloc[:, df.columns.str.startswith("k_")].isna().values.all(axis=1)
+    ].index
+    df.drop(index=not_started_indices, inplace=True)
+    print(
+        f"Original rows count: {rows_count} || New rows count: {len(df)} || Dropped Rows: {rows_count - len(df)}"
+    )
+
+    # 4. Dropping runners that have a null value in these columns [yob, gender]
+    print("Dropping rows with null values in `yob` and `gender` columns:")
+    df = drop_null_by_col(df, ["yob", "gender"])
+
+    # 5. Converting time and pace into seconds.
+    df = convert_to_sec(df, splits_keys)
+
+    # 6. Converting splits' time, pace, and speed dtype.
+    df = convert_split_dtype(df, splits_keys)
+
+    # 7. Adding the `last_split` and `race_state` columns.
+    df["last_split"] = df.iloc[:, df.columns.str.contains("time")].idxmax(axis=1)
+    df["race_state"] = np.where(
+        df["last_split"] == "k_finish_time", "Finished", "Started"
+    )
+
+    # 8.
+    # 8.1 Calculate the age based on the year of birth.
+    df["yob"] = df["yob"].apply(calc_age, args=(year,))
+    # 8.2 Dropping any non-adult runner age < 18.
+    print("** Dropping non-adult runners (age < 18):")
+    org_count = len(df)
+    df.drop(df[df["yob"] < 18].index, inplace=True)
+    dropped_count = org_count - len(df)
+    print(
+        f"Original rows count: {org_count} || New rows count: {len(df)} || Dropped rows: {dropped_count}"
+    )
+    # 8.3 Get the appropriate age category.
+    df["yob"] = df["yob"].apply(get_age_cat)
+    # 8.4 the column named `yob` changed to `age_cat`.
+    df.rename(columns={"yob": "age_cat"}, inplace=True)
+    print("** column name `yob` changed to `age_cat`.")
+    # 9. Replacing `last_split` values with the standard values.
+    df["last_split"] = df["last_split"].replace(last_split_std)
+
+    # 10. Reordering the DataFrame columns.
+    df = df[cols_order]
+
+    # 11. Convert columns into best possible dtypes (dtypes are inferred).
     df = df.convert_dtypes()
 
     return df
@@ -291,3 +391,56 @@ def get_ham_age_cat_dict(df: pd.DataFrame) -> dict:
             case _:
                 age_cat_dict[key] = value
     return age_cat_dict
+
+
+def calc_age(yob: int, cur_year: int) -> int:
+    """
+    ### Function to calculate the age from the year of birth (i.e yob).
+    ----
+    ### Arguments:
+    + yob: Year of birth.
+    + curr_year: THe year to be used to calculate the age.
+    ----
+    ### Returns the age as int.
+    """
+    # Checking if the yob is between 2000 and the curr_year.
+    if 00 <= yob <= cur_year % 100:
+        full_yob = yob + 2000
+    else:
+        full_yob = yob + 1900
+
+    return cur_year - full_yob
+
+
+def get_age_cat(age: int) -> str:
+    """
+    ### Functions to create the appropriate age category based on age; if no age category matches the age will be returned instead.
+    N.B Age categories are: `['18-39', '40-44', '45-49', '50-54', '55-59', '60-64', '65-69', '70-74', '75-79', '80+']`
+    ----
+    ### Arguments
+    + age: The age of the runner.
+    ----
+    ### Returns The age category based on the age.
+    """
+    if 18 <= age <= 39:
+        return "18-39"
+    elif 40 <= age <= 44:
+        return "40-44"
+    elif 45 <= age <= 49:
+        return "45-49"
+    elif 50 <= age <= 54:
+        return "50-54"
+    elif 55 <= age <= 59:
+        return "55-59"
+    elif 60 <= age <= 64:
+        return "60-64"
+    elif 65 <= age <= 69:
+        return "65-69"
+    elif 70 <= age <= 74:
+        return "70-74"
+    elif 75 <= age <= 79:
+        return "75-79"
+    elif age >= 80:
+        return "80+"
+    else:
+        return age
